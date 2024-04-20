7eadfde62cc937e696f346c7c4c01b29ad7d3aae
JangWoojun <woojun0107@naver.com>
2024-04-21 01:54:10 +0900

    feat: 유저 auth 페이지 이동 로직 구현

--- app/src/main/java/com/woojun/fried_chat/SplashActivity.kt
+++ app/src/main/java/com/woojun/fried_chat/SplashActivity.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.woojun.fried_chat.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         Handler().postDelayed({
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            if (auth.currentUser != null) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
             finishAffinity()
         }, 2000)
     }
