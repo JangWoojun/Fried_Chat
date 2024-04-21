a3075b238a89d90a72bde7c05fa1163f909aae6d
JangWoojun <woojun0107@naver.com>
2024-04-21 21:24:49 +0900

    feat: PublicCommunityAdapter 구현

--- app/src/main/java/com/woojun/fried_chat/public_community/PublicCommunityAdapter.kt
+++ app/src/main/java/com/woojun/fried_chat/public_community/PublicCommunityAdapter.kt
@@ -1,12 +1,17 @@
 package com.woojun.fried_chat.public_community
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.woojun.fried_chat.R
 import com.woojun.fried_chat.databinding.PublicFeedItemBinding
+import com.woojun.fried_chat.private_community.FeedChatAdapter
 
 class PublicCommunityAdapter(private val feeds: MutableList<PublicFeed>): RecyclerView.Adapter<PublicCommunityAdapter.PublicCommunityViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PublicCommunityViewHolder {
@@ -16,7 +21,25 @@ class PublicCommunityAdapter(private val feeds: MutableList<PublicFeed>): Recycl
         )
         return PublicCommunityViewHolder(binding).also { handler ->
             binding.apply {
-
+                likeButton.setOnClickListener {
+                    val feed = feeds[handler.position]
+                    if (!feed.touch) {
+                        feeds[handler.position].like = feed.like+1
+                        likeText.text = "${feed.like + 1} 개"
+                        likeImage.setImageResource(R.drawable.select_like_icon)
+                        feeds[handler.position].touch = !feed.touch
+                    } else {
+                        feeds[handler.position].like = feed.like-1
+                        likeText.text = "${feed.like - 1} 개"
+                        likeImage.setImageResource(R.drawable.like_icon)
+                        feeds[handler.position].touch = !feed.touch
+                    }
+                    notifyDataSetChanged()
+                }
+                chatButton.setOnClickListener {
+                    val chatBottomSheet = FeedChatBottomSheet(feeds[handler.position].chatList)
+                    chatBottomSheet.show((binding.root.context as AppCompatActivity).supportFragmentManager, chatBottomSheet.tag)
+                }
             }
         }
 
@@ -42,9 +65,8 @@ class PublicCommunityAdapter(private val feeds: MutableList<PublicFeed>): Recycl
                     .into(image)
                 Glide.with(binding.root.context)
                     .load(feed.feedImage)
-                    .apply(RequestOptions.circleCropTransform())
                     .apply(RequestOptions.formatOf(DecodeFormat.PREFER_ARGB_8888))
-                    .into(image)
+                    .into(feedImage)
                 nicknameText.text = feed.nickname
                 feedText.text = feed.feedText
                 likeText.text = "${feed.like}개"
